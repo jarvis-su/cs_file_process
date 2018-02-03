ad941dd66f80f34d4f89ff1fb9ab637f9f4de6c4
苏杰 <sujie@careate.cn>
2018-02-03 17:50:32 +0800

    first commit

---
+++ ConsoleApp001/ConsoleApp001/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Print any ken to continue ");
+
+            Console.WriteLine("\nWhat is your name? ");
+            var name = Console.ReadLine();
+
+            var date = DateTime.Now;
+            Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.Write("\nPress any key to exit...");
+
+            Console.ReadKey(true);
+        }
+    }
+}
